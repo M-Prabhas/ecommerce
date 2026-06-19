6b6917b16dfc3cf0b14d5aed2c87e54c0226ea80
Mutawar Prabhas <prabhasmuta99@gmail.com>
2026-06-19 18:16:38 +0530

    service discovery created

---
+++ discoveryservice/src/test/java/com/microservicesproject/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesproject.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
